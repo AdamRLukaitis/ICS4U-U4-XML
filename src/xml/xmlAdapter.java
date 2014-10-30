8dff1f81ad80512179ca1594747f8576a2f17d82
AdamRLukaitis <adam.lukaitis@gmail.com>
2014-10-30 16:19:02 -0400

    Initial NetBeans Project

---
+++ src/xml/xmlAdapter.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xml;
+
+import java.io.IOException;
+import java.util.List;
+  
+import org.jdom2.Content;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.util.IteratorIterable;
+  
+
+/**
+ *
+ * @author student1
+ */
+public class xmlAdapter {
+    
+    String xmlFileName = "xmltest1.xml";
+    String xmlValidFile = "xmlvalid1.xml";
+    
+    public void xmlData(){}
+    
+    
+    
+    
+    
+    
+}
